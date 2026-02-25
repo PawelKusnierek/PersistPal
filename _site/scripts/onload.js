938559e797169b0b529f4fb497b8a137f89b6da8
Kusnier <31161519+PawelKusnierek@users.noreply.github.com>
2026-02-25 17:40:05 +0100

    Add profession guide, change modifiers to Weapons only from Ranged/Melee

--- _site/scripts/onload.js
+++ _site/scripts/onload.js
@@ -23,20 +23,14 @@ function enable_expandable_div_buttons() {
 }
 
 function enable_default_tabs() {
-  if (location.href.split("/").slice(-1).includes('quests')) {
-    // Show Spoilers tab when present (e.g. cached/old build with tab structure)
-    if (document.getElementById("SpoilersTab")) {
-      initial_show_tab("SpoilersTab");
-    }
-  }
-  else if (location.href.split("/").slice(-1).includes('equipment')) {
-    initial_show_tab("Druid")
-  }
-  else if (location.href.split("/").slice(-1).includes('beginners-and-faq')) {
+  if (location.pathname.includes('beginners-and-faq')) {
     initial_show_tab("Beginners")
   }
-  else if (location.href.split("/").slice(-1).includes('about')) {
+  else if (location.pathname.includes('about')) {
     initial_show_tab("About")
+  }
+  else if (location.pathname.includes('profession')) {
+    initial_show_tab("Prof1")
   }
 }
 
